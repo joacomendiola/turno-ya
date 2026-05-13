dd498cea2d09c2ec41f0d7df49914c06beb03347
nehuen-hervias <nehuenhervias@gmail.com>
2026-05-13 17:08:30 -0300

    Protejo las vistas que lo requieren con LoginRequiredMixin

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 """Vistas iniciales para navegar médicos y pantalla de inicio."""
 
-from django.views.generic import ListView, TemplateView, DetailView
+from django.views.generic import CreateView, DeleteView, ListView, TemplateView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Medico, Turno, Paciente
 
 
@@ -11,37 +12,37 @@ class HomeView(TemplateView):
     context_object_name = "home_data"
 
 
-class ListaMedicosView(ListView):
+class ListaMedicosView(LoginRequiredMixin, ListView):
     """Lista todos los médicos."""
 
     model = Medico
     template_name = "clinica/lista_medicos.html"
     context_object_name = "medicos"
 
-class DetalleMedicoView(DetailView):
+class DetalleMedicoView(LoginRequiredMixin, DetailView):
     """Muestra el detalle de un médico, incluyendo su agenda."""
 
     template_name = "clinica/detalle_medico.html"
     model = Medico
     context_object_name = "medico"
 
-class ListaTurnosView(ListView):
+class ListaTurnosView(LoginRequiredMixin, ListView):
     """Lista los turnos de un médico."""
     model = Turno
     template_name = "clinica/lista_turnos.html"
     context_object_name = "turnos"
 
-class NuevoTurnoView(TemplateView):
+class NuevoTurnoView(LoginRequiredMixin, CreateView):
     """Permite crear un nuevo turno para un médico."""
 
     template_name = "clinica/nuevo_turno.html"
 
-class CancelarTurnoView(TemplateView):
+class CancelarTurnoView(LoginRequiredMixin, DeleteView):
     """Permite cancelar un turno existente."""
 
     template_name = "clinica/cancelar_turno.html"
 
-class ListaPacientesView(ListView):
+class ListaPacientesView(LoginRequiredMixin, ListView):
     """Lista los pacientes de un médico."""
 
     template_name = "clinica/lista_pacientes.html"
