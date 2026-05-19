611703133b7d202d7d0162bb3b69f3bb5ce2b5b8
joacomendiola <joakomendiola25@gmail.com>
2026-05-19 12:40:28 -0300

    modelos: manager personalizado para especialidad

--- app/models.py
+++ app/models.py
@@ -95,9 +95,20 @@ class Paciente(models.Model): pass
 class Turno(models.Model): pass
 class Ausencia(models.Model): pass
 
+
+
+
+
+
+class EspecialidadManager(models.Manager):
+    def con_medicos_activos(self):
+        return self.annotate(num_medicos=models.Count("medico")).filter(num_medicos__gt=0)
+
 class Especialidad(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
     descripcion = models.TextField(blank=True)
+
+    objects = EspecialidadManager()
     
     def __str__(self):
         return self.nombre
@@ -108,20 +119,20 @@ class Especialidad(models.Model):
             errors.append("El nombre de la especialidad es obligatorio.")
             return errors
         
-        @classmethod
-        def new(cls, **kwargs):
-            especialidad = cls(**kwargs)
-            errors = especialidad.validate()
-            if errors:
-                return None, errors
-            especialidad.save()
-            return especialidad, []
+    @classmethod
+    def new(cls, **kwargs):
+        especialidad = cls(**kwargs)
+        errors = especialidad.validate()
+        if errors:
+            return None, errors
+        especialidad.save()
+        return especialidad, []
         
-        def update(self, **kwargs):
-            for field, value in kwargs.items():
-                setattr(self, field, value)
-            errors = self.validate()
-            if errors:
-                return errors
-            self.save()
-            return []
+    def update(self, **kwargs):
+        for field, value in kwargs.items():
+            setattr(self, field, value)
+        errors = self.validate()
+        if errors:
+            return errors
+        self.save()
+        return []
